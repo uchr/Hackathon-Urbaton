bcb0863f5a9e09c83ccb67520476f37323a61304
Anton Tarasov <uchiro.me@gmail.com>
2019-05-19 13:32:46 +0700

    Create CSVToPicture.py
    script for conversion csv file to image

---
+++ greening/CSVToPicture.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import cv2
+import os
+
+
+path = './road_tiles/tiles/'
+i_max = 5.0
+rgb_green = np.array([0, 128, 0])
+
+for file in os.listdir(path):
+	file_path = os.path.join(path, file)
+	tile = pd.read_csv(file_path, sep=';')
+	tile = tile.fillna(i_max)
+
+	values = tile.values
+
+	image = np.arange(256 * 256 * 4).reshape(256, 256, 4)
+	for i in range(256):
+		for j in range(256):
+			alpha = int(255 * values[i * 256 + j][2] / i_max)
+			image[i][j] = np.array([0, 128, 0, alpha])
+	cv2.imwrite(os.path.join('./road_images/', file.split('.')[0] + '.png'), image)
